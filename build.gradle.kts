fa78fdd4576017885e6a1a71aa7638ba709771b9
Chandan Kumar <cksharma122@gmail.com>
2023-09-10 12:12:55 +0530

    Add coverage plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.0"
     application
+    jacoco
 }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 
 group = "org.gt.ridesharing"
 version = "1.0-SNAPSHOT"
@@ -21,10 +28,30 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+    extensions.configure(JacocoTaskExtension::class) {
+        excludeClassLoaders = listOf("*QuarkusClassLoader")
+        setDestinationFile(file("$buildDir/jacoco-quarkus.exec"))
+        isEnabled = true
+
+    }
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "17"
 }
 
 application {
